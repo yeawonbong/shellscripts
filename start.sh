05f4ae36a91722e4e016c6728af9e34649965a69
ybong <75327385+yeawonbong@users.noreply.github.com>
2021-11-05 18:19:35 +0900

    Update start.sh
    simpler

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "start: cloning repos in goinfre.."
+cd ../goinfre
 git clone https://github.com/yeawonbong/yeawonbong.github.io.git
 git clone https://github.com/yeawonbong/philosophers.git
-mv yeawonbong.github.io ../goinfre
-mv philosophers ../goinfre
 
 exit 0

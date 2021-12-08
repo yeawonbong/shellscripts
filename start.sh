7b63f6cbf4583dc31239b9f6ebb1c90877fb560f
Yeawon Bong <ybong@c5r1s7.42seoul.kr>
2021-12-08 13:37:58 +0900

    update

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 echo "start: cloning repos in goinfre.."
 cd ../goinfre
 git clone https://github.com/yeawonbong/yeawonbong.github.io.git
-git clone https://github.com/yeawonbong/philosophers.git
+git clone https://github.com/yeawonbong/cpp.git
 
 exit 0
